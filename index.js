d79c4a5058b3f4cbdc86d5f1dc67c2cf6f2abc53
Abdulmalik <hazardgamin9@gmail.com>
2022-02-01 23:06:16 +0000

    added fs require & changed index.js body

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var http = require("http");
+fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
     // Send the HTTP header
     // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // Content Type: text/html
 
-    //Send the response body as "Hello World"
-    response.end('Hello World\n');
+    // write to our server. set configuration for the response
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '*'
+    });
+
+    var readStream = fs.createReadStream(__dirname + '/index.html');
+    // send a message
+    readStream.pipe(response);
+
 }).listen(PORT);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:' + PORT);
