b2756a732ef71aaac0cb7c145be2f4e7539a92a2
Abdulmalik <hazardgamin9@gmail.com>
2022-02-06 22:25:29 +0000

    Route Middleware (router.use())

--- server.js
+++ server.js
@@ -25,9 +25,16 @@ adminRouter.get('/posts', function(req, res) {
 // apply the routes to our application
 app.use('/admin', adminRouter);
 ///////////////////////////////////////////////////////////////////
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    // continue doing what we were doing and go to the route
+    next(); });
+///////////////////////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////////////////// start the server
+
+///////////////////////////////////////////////////////////////////
 app.listen(PORT);
-console.log('Express Server running at http://127.0.0.1:'.PORT);
-console.log("Hi");
+console.log('Express Server running at http://127.0.0.1:' .PORT);
 ///////////////////////////////////////////////////////////////////
