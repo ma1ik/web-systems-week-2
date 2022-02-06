efd4315b64ff5e2a92513288bb3dbc4dfb776651
Abdulmalik <hazardgamin9@gmail.com>
2022-02-06 21:37:39 +0000

    updated small mistake

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.use('/admin', adminRouter);
 
 /////////////////////////////////////////////////////////////////// start the server
 app.listen(PORT);
-console.log('Express Server running at http://127.0.0.1:'.PORT);
+console.log('Express Server running at http://127.0.0.1:'+ PORT);
 ///////////////////////////////////////////////////////////////////
