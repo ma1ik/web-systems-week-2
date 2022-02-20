109234f6426fdf35b4da05818888b808c8cd2df3
Abdulmalik <hazardgamin9@gmail.com>
2022-02-20 18:04:31 +0000

    Reached writing to the database

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // load the express package and create our app
 var express = require('express');
 var app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 // set the port based on environment (more on environments later)
-var port = PORT;
+/* Week 3 Stacks Lab, connecting MongoDB */
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://127.0.0.1:27017";
+
+
+
+
 // send our index.html file to the user for the home page
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -52,25 +58,35 @@ adminRouter.get('/users/:name', function(req, res) {
 ///////////////////////////////////////////////////////////////////
 app.route('/login')
     // show the form (GET http://localhost:PORT/login)
-    .get(function(req, res) {       var output = 'getting the login! ';
-        var input1 = req.query['input1'];
-        var input2 = req.query['input2'];
-        if (typeof input1 != 'undefined' && typeof input2 != 'undefined') {
-            output+=('There was input: ' + input1 + ' and ' + input2);
-            res.send(output);
-        }
-        console.log('Start the database stuff');
+    .get(function(req, res) {
+        var output = 'processing the login form';
+        var input1 = req.query.input1;
+        var input2 = req.query.input2;
+        console.log('The params:' + req.query.input1 + " " + req.query.input2);
+
+        res.send('this is the login form');
+        /* Connecting to Mongo client */
+        MongoClient.connect(uri, function (err, db) {
+            if(err) throw err;
+            console.log('Start the database stuff');
+            //Write database Insert/Update/Query code here..
+            var dbo = db.db("mydb");
+            var userInput = { firstInput: "users", secondInput: "secondUser" };
+            dbo.collection("users").insertOne(userInput, function(err, res) {
+                if (err) throw err;
+                console.log("1 user inserted");
+                db.close();
+            });
+            console.log('End the database stuff');
+            });
     })
     // process the form (POST http://localhost:PORT/login)
     .post(function(req, res) { console.log('processing');
         res.send('processing the login form!');
     });
 
-///////////////////////////////////////////////////////////////////
-
-
 /////////////////////////////////////////////////////////////////// start the server
 app.listen(PORT);
-console.log('Express Server running at http://127.0.0.1:'.PORT);
+console.log('Express Server running at http://127.0.0.1:'+PORT);
 
 ///////////////////////////////////////////////////////////////////
